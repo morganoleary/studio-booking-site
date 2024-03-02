b0ff16ebf1251694bc065c79446516e98bb66d5e
Morgan Oleary <morganleeoleary@gmail.com>
2024-03-02 23:07:07 +0000

    Secure secret key

--- soul_base/settings.py
+++ soul_base/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g9egkb!i0ondm&fq-3n30ce^@hg*1#2#4&3l5&%c3aeqa8iugp'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
