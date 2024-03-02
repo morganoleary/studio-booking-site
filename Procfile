c0b4675261c007f8a27b0f6e9ada6821cd133d1d
Morgan Oleary <morganleeoleary@gmail.com>
2024-03-02 20:55:27 +0000

    Install gunicorn for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn soul_base.wsgi
--- soul_base/settings.py
+++ soul_base/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g9egkb!i0ondm&fq-3n30ce^@hg*1#2#4&3l5&%c3aeqa8iugp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
